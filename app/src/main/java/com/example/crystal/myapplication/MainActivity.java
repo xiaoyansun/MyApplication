5ca99674682c90879c4436b7ad77598df342abf7
xiaoyansun <xiaoyansun2015@gmail.com>
2018-12-01 17:05:49 -0500

    test voice bot

---
+++ app/src/main/java/com/example/crystal/myapplication/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.crystal.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.AWSStartupHandler;
+import com.amazonaws.mobile.client.AWSStartupResult;
+
+import com.amazonaws.mobileconnectors.lex.interactionkit.Response;
+import com.amazonaws.mobileconnectors.lex.interactionkit.config.InteractionConfig;
+import com.amazonaws.mobileconnectors.lex.interactionkit.ui.InteractiveVoiceView;
+
+import java.util.Locale;
+import java.util.Map;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    public static String LOG_TAG = "tag";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AWSMobileClient.getInstance().initialize(this, new AWSStartupHandler() {
+            @Override
+            public void onComplete(AWSStartupResult awsStartupResult) {
+                Log.d("YourMainActivity", "AWSMobileClient is instantiated and you are connected to AWS!");
+            }
+        }).execute();
+
+        init();
+
+    }
+
+    public void init(){
+        InteractiveVoiceView voiceView =
+                (InteractiveVoiceView) findViewById(R.id.voiceInterface);
+
+
+
+        voiceView.setInteractiveVoiceListener(
+                new InteractiveVoiceView.InteractiveVoiceListener() {
+
+                    @Override
+                    public void dialogReadyForFulfillment(Map slots, String intent) {
+                        Log.d(LOG_TAG, String.format(
+                                Locale.US,
+                                "Dialog ready for fulfillment:\n\tIntent: %s\n\tSlots: %s",
+                                intent,
+                                slots.toString()));
+                    }
+
+                    @Override
+                    public void onResponse(Response response) {
+                        Log.d(LOG_TAG, "Bot response: " + response.getTextResponse());
+                    }
+
+                    @Override
+                    public void onError(String responseText, Exception e) {
+                        Log.e(LOG_TAG, "Error: " + responseText, e);
+                    }
+                });
+
+        voiceView.getViewAdapter().setCredentialProvider(AWSMobileClient.getInstance().getCredentialsProvider());
+
+        //replace parameters with your botname, bot-alias
+        voiceView.getViewAdapter()
+                .setInteractionConfig(
+                        new InteractionConfig("BookTripMOBILEHUB","test"));
+
+        voiceView.getViewAdapter()
+                .setAwsRegion(getApplicationContext()
+                        .getString(R.string.aws_region));
+    }
+}
